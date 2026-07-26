cabe2457c9c46df88d304692cb3d418bb04f6ed1
ntrllog <ntrllog@gmail.com>
2026-07-25 19:44:02 -0700

    update version code, name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ntrllog.github.io.pinger"
         minSdk = 21
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
